95947d272b92808e65934e181cdccf7b572fd2f2
Alexis Newby <alexis.newby@gmail.com>
2021-11-09 16:56:54 -0700

    Build out skeletons of classes and note what needs to be done or belongs in each file

---
+++ game.js
@@ -0,0 +1,13 @@
+class Game {
+  constructor() {
+    
+  }
+}
+
+
+// includes:
+//   2 Player instances
+//   A way to keep track of the data for the game board
+//   A way to keep track of the selected game type
+//   A way to detect when a game is a draw (no one has won)
+//   A way to reset the Game’s board to begin a new game
