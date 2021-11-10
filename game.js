dad74e4c53431f260de3a07453bd25fcb7b9eb5a
Alexis Newby <alexis.newby@gmail.com>
2021-11-10 08:00:43 -0700

    Build out the skeleton for the game play view

--- game.js
+++ game.js
@@ -1,12 +1,12 @@
 class Game {
   constructor() {
-    
+
   }
 }
 
 
 // includes:
-//   2 Player instances
+//   2 Player instances (humanPlayer, computerPlayer)
 //   A way to keep track of the data for the game board
 //   A way to keep track of the selected game type
 //   A way to detect when a game is a draw (no one has won)
