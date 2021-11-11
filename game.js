313b4ca8b011c99782b11c58e5ca7e4eb7806768
Alexis Newby <alexis.newby@gmail.com>
2021-11-11 09:46:39 -0700

    Continue building out classes

--- game.js
+++ game.js
@@ -1,26 +1,48 @@
-var Player = require('./Player');
-
 class Game {
   constructor(type) {
-//    this.gameboardData = {}
-    this.type = type
+    this.human = new Player('Human', 😄);
+    this.computer = new Computer('Computer', 💻)
+    this.message ='Choose your game!'
+    this.type = type;
+    //game must time out
   }
-  initializeNewGame() {
-    var human = new Player('Human', token, 0);
-    var computer = new Player('Computer', token, 0);
-  }
+  // initializeNewGame() {
+  //   var human = new Player('Human', token, 0);
+  //   var computer = new Player('Computer', token, 0);
+  // }
+  //I think this code is redundant.
   resetGameBoard() {
     //set timeout function;
     //go to the main view of the specific board
   }
+  determineGameType() {
+    if (this.type === 'buddy') {
+      determineWinnerBasic(buddy)
+    } else {
+      determineWinnerBeast(buddy)
+    }
+    //I don't yet know where 'budy'/ type in constructor is coming from. Needs to connect to the button selected.
+  }
+  determineWinnerBasic(buddy) {
+    this.human.takeTurnBasic(buddy);
+    this.computer.takeTurnBasic();
+    //game logic here
+  }
+  determineWinnerBeast(buddy) {
+    this.human.takeTurnBeast(buddy);
+    this.computer.takeTurnBasic();
+    //game logic here
+  }
 }
 
 
+
 // includes:
-//   2 Player instances (humanPlayer, computerPlayer)
+//   2 Player instances (human, computer)
 //   A way to keep track of the data for the game board
 //   A way to keep track of the selected game type
 //   A way to detect when a game is a draw (no one has won)
+      //I beleive this could also be a big conditional that determines who wins and loses.
 //   A way to reset the Game’s board to begin a new game
 
 
@@ -34,3 +56,7 @@ class Game {
 //if game is selected - 1 set of rules
 
 //if harder game is selected - 1 other set of rules
+
+
+//Game logic:
+//human wins if
--- player.js
+++ player.js
@@ -1,16 +1,44 @@
 class Player {
-  constructor(name, token, wins) {
+  constructor(name, token) {
     this.name = name;
     this.token = token;
-    this.wins = wins;
+    this.wins = 0;
+    this.buddy
   }
   saveWinsToStorage() {
-
+    //localStorage.setItem()
   }
   retrieveWinsFromStorage() {
-
+    //localStorage.getItem()
   }
-  takeTurn() {
-    
+  takeTurnBasic(buddy) {
+    if (buddy) {
+      this.buddy = buddy
+    } else {
+      var buddies = ['bear', 'bee', 'bowling ball'];
+      var getRandomIndex = Math.floor(Math.random() * 3);
+      this.buddy = buddies[getRandomIndex]
+    }
+  }
+  takeTurnBeast(buddy) {
+    if (buddy) {
+      this.buddy = buddy
+    } else {
+      var buddies = ['bear', 'bee', 'bowling ball', 'brunch', 'bicycle'];
+      var getRandomIndex = Math.floor(Math.random() * 5);
+      this.buddy = buddies[getRandomIndex]
+    }
   }
 }
+
+
+
+//takeTurn notes:
+//human takes a turn and computer takes a turn -> this happens in the GAME
+//human needs a buddy
+//computer needs a buddy
+//
+// from romcom:
+// function getRandomIndex(array) {
+//   return Math.floor(Math.random() * array.length);
+// }
