572340b65518c6a81a2c5214cee9c57345d5229a
Alexis Newby <alexis.newby@gmail.com>
2021-11-12 22:29:26 -0700

    Change nothing, tinkering with append

--- player.js
+++ player.js
@@ -15,16 +15,16 @@ class Player {
   }
 
   takeTurn(buddy) {
-    var buddies = ['brunch', 'bike', 'bear', 'bee', 'bowling'];
+    var buddyOptions = ['brunch', 'bike', 'bear', 'bee', 'bowling'];
     if (buddy) {
       this.buddy = buddy
       console.log(this.buddy)
     } else if (game.type === 'beast'){
       var randomBuddyIndex = Math.floor(Math.random() * 5);
-      this.buddy = buddies[randomBuddyIndex]
+      this.buddy = buddyOptions[randomBuddyIndex]
       console.log(this.buddy)
     } else {
-      var basicVersion = buddies.slice(2)
+      var basicVersion = buddyOptions.slice(2)
       var basicRandomBuddyIndex = Math.floor(Math.random() * 3);
       this.buddy = basicVersion[basicRandomBuddyIndex]
       console.log(this.buddy)
