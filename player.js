23145df80de110ee240404330a432472f0da03b8
Alexis Newby <alexis.newby@gmail.com>
2021-11-15 12:57:51 -0700

    Fix misunderstanding about DOM on page load

--- player.js
+++ player.js
@@ -19,16 +19,13 @@ class Player {
     var buddyOptions = ['brunch', 'bike', 'bear', 'bee', 'bowling'];
     if (buddy) {
       this.buddy = buddy;
-      console.log(this.buddy)
     } else if (game.type === 'beast'){
       var randomBuddyIndex = Math.floor(Math.random() * 5);
       this.buddy = buddyOptions[randomBuddyIndex];
-      console.log(this.buddy)
     } else {
       var basicVersion = buddyOptions.slice(2);
       var basicRandomBuddyIndex = Math.floor(Math.random() * 3);
       this.buddy = basicVersion[basicRandomBuddyIndex];
-      console.log(this.buddy)
     }
   }
 };
